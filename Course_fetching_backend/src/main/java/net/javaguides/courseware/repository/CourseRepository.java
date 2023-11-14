40efe98235bec7e8c63b9b9873af124a4fec0769
Pritam Nayak <113450331+Pritam943@users.noreply.github.com>
2023-11-14 19:20:32 +0530

    Delete Course_fetching_backend directory

--- Course_fetching_backend/src/main/java/net/javaguides/courseware/repository/CourseRepository.java
+++
@@ -1,9 +0,0 @@
-package net.javaguides.courseware.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import net.javaguides.courseware.entity.course;
-
-public interface CourseRepository extends JpaRepository<course, Long> {
-
-}
